750585f869c6e29acb20e122ac2c13669727cbf6
Nath <nathaliasarto3@gmail.com>
2021-04-21 14:30:25 -0300

    código final di curso

--- Loops.js
+++ Loops.js
@@ -8,7 +8,7 @@ const listaDestinos = new Array(
 const idadeComprador = 19;
 const estaAcompanhada = false;
 let comprouPassagem = false;
-const destino = "Salvador";
+const destino = "Paris";
 
 console.log("\nDestinos Possíveis: ");
 console.log(listaDestinos);
@@ -17,13 +17,37 @@ console.log(listaDestinos);
 const podeComprar = (idadeComprador >= 18 || estaAcompanhada == true);
 
 let contador = 0;
-while (contador < 3) {
-    if (listaDestinos[contador] == destino){
-        console.log("Destino é real")
-    } else {
-        console.log("Destino não é real")
+let destinoExiste = false;
+
+// while (contador < 3) {
+
+//     if (listaDestinos[contador] == destino) {
+//         //console.log("Destino é real")
+//         destinoExiste = true;
+//         break;
+//     }
+//     //if(contador ==2 && !destinoExiste){
+//     //     console.log("Destino não é real")
+//     // }
+//     contador += 1;
+// }
+
+
+
+for (let cont = 0; cont < 3; cont++) {
+    if (listaDestinos[contador] == destino) {
+        //console.log("Destino é real")
+        destinoExiste = true;
+        break;
     }
-    contador += 1;
+}
+
+console.log("Destino existe: ", destinoExiste);
+
+if (podeComprar && destinoExiste) {
+    console.log("Boa viagemmm")
+} else {
+    console.log("Sorry, honey")
 }
 
 
