b426338333f9c25c092cae136a820eb5ea0a2d39
Nath <nathaliasarto3@gmail.com>
2021-04-24 23:03:06 -0300

    Meu primeiro programinha

---
+++ POO/index.js
@@ -0,0 +1,11 @@
+const cliente1Nome = "Nathalia";
+const cliente1CPF = 123456789;
+const cliente1Agencia = 1001;
+const cliente1Slado = 0;
+
+const cliente2Nome = "Waliks";
+const cliente2CPF = 987654321;
+const cliente2Agencia = 1001;
+const cliente2Slado = 50000;
+
+console.log(cliente1Nome);
